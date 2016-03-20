f52624406b80b8ca7a3fce875db5c4359873e565
luisjacome <luis1987_m@live.com>
2016-03-20 17:12:25 -0500

    longuitud de clave

--- scripts.js
+++ scripts.js
@@ -8,8 +8,18 @@ alert(Gracias por visitar")
 console.log("Fin de programa");*/
 
 
-
+/*
 console.log("Inicio de programa");
 var equipo= prompt("Cual es tu equipo favorito?");
 document.write("El equipo es:" + equipo);
-console.log("Fin de programa");
+console.log("Fin de programa");*/
+
+/*var clave =prompt("Hola Usuario,Ingrese su clave");
+console.log("La longitud de la clave es:"+clave.length);*/
+
+var clave =prompt("Hola Usuario,Ingrese su clave en minusculas!");
+console.log("La longitud de la clave es:"+ clave.length);
+ +console.log("TA-DA!!! Su clave es: " + clave.toUpperCase());
+ +console.log("El valor original de la clave es:" + clave); 
+
+
