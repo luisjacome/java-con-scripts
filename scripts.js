611b3eec3d8e78e219ed811e72ee26a835148a0b
luisjacome <luis1987_m@live.com>
2016-03-20 16:41:27 -0500

    trabajando con variables

--- scripts.js
+++ scripts.js
@@ -1,8 +1,15 @@
-console.log("Inicio de programa");
+/*console.log("Inicio de programa");
 alert("hola javaScript");
 document.write("<h1>HOLA javaScript</P>");
 alert("Gracias por visitar");
 
 document write("<h1>HOLA javaScript</P>")
 alert(Gracias por visitar")
+console.log("Fin de programa");*/
+
+
+
+console.log("Inicio de programa");
+var equipo= prompt("Cual es tu equipo favorito?");
+document.write("El equipo es:" + equipo);
 console.log("Fin de programa");
