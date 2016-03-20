d25792fc0eed1de2aae504624127252a72a67a02
luisjacome <luis1987_m@live.com>
2016-03-20 15:06:51 -0500

    java con errores

---
+++ scripts.js
@@ -0,0 +1,8 @@
+console.log("Inicio de programa");
+alert("hola javaScript");
+document.write("<h1>HOLA javaScript</P>");
+alert("Gracias por visitar");
+
+document write("<h1>HOLA javaScript</P>")
+alert(Gracias por visitar")
+console.log("Fin de programa");
